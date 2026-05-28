b9ef88f056b0e418341c02d37428915b1a7b4fed
Lynette Grace L. Galindon <galindonlynette@gmail.com>
2026-05-28 09:44:22 +0000

    added folders

---
+++ backend/src/test/java/com/edutask/interim/InterimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edutask.interim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
